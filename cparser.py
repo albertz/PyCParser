e8484a888fb16ab09e28e3c307af2e8ed09843f7
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-06-21 17:02:18 +0200

    more: cpreprocess_evaluate_cond and stuff

--- cparser.py
+++ cparser.py
@@ -3,8 +3,27 @@ SpaceChars = " \t"
 LowercaseLetterChars = "abcdefghijklmnopqrstuvwxyz"
 LetterChars = LowercaseLetterChars + LowercaseLetterChars.upper()
 NumberChars = "0123456789"
+OpChars = "&|=!+-"
 
+def simple_escape_char(c):
+	if c == "n": return "\n"
+	elif c == "t": return "\t"
+	else: return c
+
+class Macro:
+	def __init__(self, args, string):
+		self.args = args
+		self.string = string
+		self.func = lambda: "" # TODO...
+	def __str__(self):
+		return "(" + ", ".join(self.args) + ") -> " + self.string
+	def __call__(self, *args):
+		if len(args) != len(self.args): raise TypeError, "invalid number of args in " + str(self)
+		return self.func(*args)
+		
 class State:
+	EmptyMacro = Macro((), "")
+	
 	def __init__(self):
 		self.macros = {} # name -> func
 		self._preprocessIfLevels = []
@@ -13,6 +32,14 @@ class State:
 		self._preprocessIncludeLevel = []
 		self._errors = []
 	
+	def autoSetupSystemMacros(self):
+		import sys
+		if sys.platform == "darwin":
+			self.macros["__APPLE__"] = self.EmptyMacro
+			self.macros["__MACH__"] = self.EmptyMacro
+			self.macros["__MACOSX__"] = self.EmptyMacro
+			self.macros["i386"] = self.EmptyMacro
+			
 	def incIncludeLineChar(self, fullfilename=None, inc=None, line=None, char=None, charMod=None):
 		CharStartIndex = 0
 		LineStartIndex = 1
@@ -75,6 +102,7 @@ class State:
 		self._preprocessIncludeLevel = self._preprocessIncludeLevel[:-1]
 
 def is_valid_defname(defname):
+	if not defname: return False
 	gotValidPrefix = False
 	for c in defname:
 		if c in LetterChars + "_":
@@ -92,10 +120,241 @@ def cpreprocess_evaluate_ifdef(state, arg):
 		return False
 	return arg in state.macros
 
-def cpreprocess_evaluate_cond(state, condstr):
-	# TODO ...
-	state.error("preprocessor: if-evaluation not yet implemented; cannot check '" + condstr + "'")
-	return True # may be more often what we want :P
+def cpreprocess_evaluate_single(state, arg):
+	if arg == "": return None	
+	try: return int(arg) # is integer?
+	except: pass
+	if len(arg) >= 2 and arg[0] == '"' and arg[-1] == '"': return arg[1:-1] # is string?
+	
+	if not is_valid_defname(arg):
+		state.error("preprocessor eval single: '" + arg + "' is not a valid macro name")
+		return 0
+	if arg not in state.macros:
+		state.error("preprocessor eval single: '" + arg + "' is unknown")
+		return 0
+	try:
+		resolved = state.macros[arg]()
+	except Exception, e:
+		state.error("preprocessor eval single error on '" + arg + "': " + str(e))
+		return 0
+	return cpreprocess_evalute_cond(state, resolved)
+
+def cpreprocess_evaluate_cond(stateStruct, condstr):
+	state = 0
+	bracketLevel = 0
+	substr = ""
+	laststr = ""
+	lasteval = None
+	op = None
+	prefixOp = None
+	opstr = ""
+	args = []
+	for c in condstr:
+		breakLoop = False
+		while not breakLoop:
+			breakLoop = True
+			
+			if state == 0:
+				if c == "(":
+					if laststr == "":
+						state = 1
+						bracketLevel == 1
+					else:
+						state = 10
+						breakLoop = False
+				elif c == ")":
+					stateStruct.error("preprocessor: runaway ')'")
+					return
+				elif c in SpaceChars:
+					if laststr != "": state = 10
+					else: pass
+				elif c in OpChars:
+					state = 10
+					breakLoop = False
+				elif c == '"':
+					if laststr == "":
+						state = 20
+					else:
+						stateStruct.error("preprocessor: '\"' not expected")
+						return
+				else:
+					laststr += c
+			elif state == 1: # in bracket
+				if c == "(":
+					bracketLevel += 1
+				if c == ")":
+					bracketLevel -= 1
+					if bracketLevel == 0:
+						neweval = cpreprocess_evaluate_cond(stateStruct, substr)
+						state = 18
+						if prefixOp is not None:
+							neweval = prefixOp(neweval)
+							prefixOp = None
+						if op is not None: lasteval = op(lasteval, neweval)
+						else: lasteval = neweval
+					else: # bracketLevel > 0
+						substr += c
+				elif c == '"':
+					state = 2
+					substr += c
+				else:
+					substr += c
+			elif state == 2: # in str in bracket
+				substr += c
+				if c == "\\": state = 3
+				elif c == '"': state = 1
+				else: pass
+			elif state == 3: # in escape in str in bracket
+				substr += c
+				state = 2
+			elif state == 10: # after identifier
+				if c in SpaceChars: pass
+				elif c in OpChars:
+					if laststr != "":
+						neweval = cpreprocess_evaluate_single(stateStruct, laststr)
+						if prefixOp is not None:
+							neweval = prefixOp(neweval)
+							prefixOp = None
+						if op is not None: lasteval = op(lasteval, neweval)
+						else: lasteval = neweval
+						laststr = ""
+					opstr = ""
+					state = 18
+					breakLoop = False
+				elif c == "(":
+					state = 11
+					bracketLevel = 1
+					args = []
+				else:
+					stateStruct.error("preprocessor eval: '" + c + "' not expected")
+					return
+			elif state == 11: # after "(" after identifier
+				if c == "(":
+					if len(args) == 0: args = [""]
+					args[-1] += c
+					bracketLevel += 1
+					state = 12
+				elif c == ")":
+					macroname = laststr
+					if macroname == "defined":
+						if len(args) != 1:
+							stateStruct.error("preprocessor eval defined-check args invalid: " + str(args))
+							return
+						else:
+							macroname = args[0]
+							if not is_valid_defname(macroname):
+								stateStruct.error("preprocessor eval defined-check: '" + macroname + "' is not a valid macro name")
+								return
+							neweval = macroname in stateStruct.macros
+					else:
+						if not is_valid_defname(macroname):
+							stateStruct.error("preprocessor eval call: '" + macroname + "' is not a valid macro name")
+							return
+						if arg not in stateStruct.macros:
+							stateStruct.error("preprocessor eval call: '" + macroname + "' is unknown")
+							return
+						macro = stateStruct.macros[macroname]
+						try:
+							resolved = macro(args)
+						except Exception, e:
+							stateStruct.error("preprocessor eval call on '" + macroname + "': error " + str(e))
+							return
+						neweval = cpreprocess_evaluate_cond(stateStruct, resolved)
+					
+					if prefixOp is not None:
+						neweval = prefixOp(neweval)
+						prefixOp = None
+					oldlast = lasteval
+					if op is not None: lasteval = op(lasteval, neweval)
+					else: lasteval = neweval
+					#print "after ):", laststr, args, neweval, op.func_code.co_firstlineno if op else "no-op", oldlast, "->", lasteval
+					laststr = ""
+					state = 18
+				elif c == '"':
+					if len(args) == 0: args = [""]
+					args[-1] += c
+					state = 13
+				elif c == ",": args += [""]
+				else:
+					if len(args) == 0: args = [""]
+					args[-1] += c
+			elif state == 12: # in additional "(" after "(" after identifier
+				args[-1] += c
+				if c == "(": bracketLevel += 1
+				elif c == ")":
+					bracketLevel -= 1
+					if bracketLevel == 1: state = 11
+				elif c == '"': state = 13
+				else: pass
+			elif state == 13: # in str after "(" after identifier
+				args[-1] += c
+				if c == "\\": state = 14
+				elif c == '"':
+					if bracketLevel > 1: state = 12
+					else: state = 11
+				else: pass
+			elif state == 14: # in escape in str after "(" after identifier
+				args[-1] += c
+				state = 13
+			elif state == 18: # op after identifier/expression
+				if c in OpChars: opstr += c
+				else:
+					if opstr == "":
+						if c in SpaceChars: pass
+						else:
+							stateStruct.error("expected op but got '" + c + "'")
+							return
+					else:
+						if opstr == "!=": op = lambda x,y: x != y
+						elif opstr == "==": op = lambda x,y: x == y
+						elif opstr == "&&": op = lambda x,y: x and y
+						elif opstr == "||": op = lambda x,y: x or y
+						elif opstr == "!":
+							newprefixop = lambda x: not x
+							if prefixOp: prefixOp = lambda x: prefixOp(newprefixop(x))
+							else: prefixOp = newprefixop
+						else:
+							stateStruct.error("invalid op '" + opstr + "' with '" + c + "' following")
+							return
+						opstr = ""
+						state = 0
+						breakLoop = False
+			elif state == 20: # in str
+				if c == "\\": state = 21
+				elif c == '"':
+					state = 0
+					neweval = laststr
+					laststr = ""
+					if prefixOp is not None:
+						neweval = prefixOp(neweval)
+						prefixOp = None
+					if op is not None: lasteval = op(lasteval, neweval)
+					else: lasteval = neweval
+				else: laststr += c
+			elif state == 21: # in escape in str
+				laststr += simple_escape_char(c)
+				state = 20
+			else:
+				stateStruct.error("internal error in preprocessor evaluation: state " + str(state))
+				return
+	
+	if state in (0,10):
+		if laststr != "":
+			neweval = cpreprocess_evaluate_single(stateStruct, laststr)
+			if prefixOp is not None:
+				neweval = prefixOp(neweval)
+				prefixOp = None
+			if op is not None: lasteval = op(lasteval, neweval)
+			else: lasteval = neweval
+	elif state == 18: # expected op
+		if opstr != "":
+			stateStruct.error("preprocessor eval: unfinished op: '" + opstr + "'")
+		else: pass
+	else:
+		stateStruct.error("preprocessor eval: invalid argument: '" + condstr + "'")
+	
+	print "eval:", condstr, "->", lasteval
+	return lasteval
 
 def cpreprocess_handle_include(state, arg):
 	arg = arg.strip()
@@ -111,7 +370,6 @@ def cpreprocess_handle_include(state, arg):
 	else:
 		state.error("invalid include argument: '" + arg + "'")
 		return
-	print "include", arg
 	for c in state.preprocess_file(filename=filename, local=local): yield c
 
 def cpreprocess_handle_def(state, arg):
@@ -295,6 +553,7 @@ def test():
 	better_exchook.install()
 	
 	state = State()
+	state.autoSetupSystemMacros()
 	s = "".join(state.preprocess_file("/Library/Frameworks/SDL.framework/Headers/SDL.h", local=True)).encode("utf-8")
 
 	return state
