155829947b05d752fed4ff8a4a4e88a373c0a894
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-07-06 14:30:32 +0200

    small fix for caching. also remember defined/ifdef/ifndef checks on macros

--- caching.py
+++ caching.py
@@ -227,6 +227,14 @@ class StateDictWrapper:
 			if not k in self._addSet: # we only care about it if we didn't add it ourself
 				self._accessSet.add(k)
 		return self._dict[k]
+	def __contains__(self, k): return self.has_key(k)
+	def has_key(self, k):
+		haskey = self._dict[k].has_key(k)
+		if haskey and self._accessSet is not None:
+			assert self._addSet is not None
+			if not k in self._addSet: # we only care about it if we didn't add it ourself
+				self._accessSet.add(k)
+		return haskey
 	def pop(self, k):
 		self._dict.pop(k)
 		self._addList.append((k,None))
