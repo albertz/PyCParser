d41e7608e5f67eaa1a5f2d4dab7e49c7a1e089c7
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-07-13 17:13:43 +0200

    more PyAST translation code

--- interpreter.py
+++ interpreter.py
@@ -31,6 +31,7 @@ def iterIdWithPostfixes(name):
 class FuncEnv:
 	def __init__(self, stateStruct):
 		self._stateStruct = stateStruct
+		self.outerScope = stateStruct
 		self.vars = {} # name -> varDecl
 		self.varNames = {} # id(varDecl) -> name
 		self.scopeStack = [] # FuncCodeblockScope
@@ -39,12 +40,25 @@ class FuncEnv:
 		assert varDecl is not None
 		assert id(varDecl) not in self.varNames
 		for name in iterIdWithPostfixes(varName):
-			if name not in self.vars:
+			if self.searchVarName(name) is None:
 				self.vars[name] = varDecl
 				self.varNames[id(varDecl)] = name
 				return name
+	def searchVarName(self, varName):
+		if varName in self.vars: return self.vars[varName]
+		if varName in self.outerScope.vars: return self.outerScope.vars[varName]
+		return None
 	def registerNewVar(self, varName, varDecl):
 		self.scopeStack[-1].registerNewVar(varName, varDecl)
+	def getAstNodeForVarDecl(self, varDecl):
+		assert varDecl is not None
+		if id(varDecl) in self.varNames:
+			# local var
+			name = self.varNames[id(varDecl)]
+			return ast.Name(id=name)
+		# we expect this is a global
+		assert varDecl.name in self.outerScope.vars, str(varDecl) + " is expected to be a global var"
+		return ast.Name(id=varDecl.name)
 	def _unregisterVar(self, varName):
 		varDecl = self.vars[varName]
 		del self.varNames[id(varDecl)]
@@ -61,8 +75,11 @@ def getAstNodeForVarType(varDecl):
 	# TODO
 	pass
 
-def makeAstNodeCall(baseExpr):
-	return ast.Call(func=baseExpr)
+def makeAstNodeCall(func, *args):
+	if not isinstance(func, ast.AST):
+		# TODO ...
+		pass
+	return ast.Call(func=func, args=args)
 
 class FuncCodeblockScope:
 	def __init__(self, funcEnv):
@@ -85,6 +102,156 @@ class FuncCodeblockScope:
 			self.funcEnv._unregisterVar(varName)
 		self.varNames.clear()
 		self.funcEnv.astNode.body.extend(astCmds)
+
+OpUnary = {
+	"~": ast.Invert,
+	"!": ast.Not,
+	"+": ast.UAdd,
+	"-": ast.USub,
+}
+
+OpBin = {
+	"+": ast.Add,
+	"-": ast.Sub,
+	"*": ast.Mult,
+	"/": ast.Div,
+	"%": ast.Mod,
+	"<<": ast.LShift,
+	">>": ast.RShift,
+	"|": ast.BitOr,
+	"^": ast.BitXor,
+	"&": ast.BitAnd,
+}
+
+OpBinBool = {
+	"&&": ast.And,
+	"||": ast.Or,
+}
+
+OpBinCmp = {
+	"==": ast.Eq,
+	"!=": ast.NotEq,
+	"<": ast.Lt,
+	"<=": ast.LtE,
+	">": ast.Gt,
+	">=": ast.GtE,
+}
+
+OpAugAssign = dict(map(lambda (k,v): (k + "=", v), OpBin.iteritems()))
+
+def helper_prefixInc(a):
+	a.value += 1
+	return a
+
+def helper_prefixDec(a):
+	a.value -= 1
+	return a
+
+def helper_postfixInc(a):
+	b = a.__class__(a.value) # copy
+	a.value += 1
+	return b
+
+def helper_postfixDec(a):
+	b = a.__class__(a.value) # copy
+	a.value -= 1
+	return b
+
+def astForStatement(funcEnv, stmnt):
+	if isinstance(stmnt, (CVarDecl,CFuncArgDecl)):
+		return funcEnv.getAstNodeForVarDecl(stmnt)
+	elif isinstance(stmnt, CStatement):
+		return astForCStatement(funcEnv, stmnt)
+	elif isinstance(stmnt, CAttribAccessRef):
+		a = ast.Attribute()
+		a.value = astForStatement(funcEnv, stmnt.base)
+		a.attr = ast.Name(id=stmnt.name)
+		return a
+	elif isinstance(stmnt, CNumber):
+		return ast.Num(n=stmnt.content)
+	elif isinstance(stmnt, CStr):
+		return ast.Str(s=stmnt.content)
+	elif isinstance(stmnt, CChar):
+		return ast.Str(s=stmnt.content)
+	elif isinstance(stmnt, CFuncCall):
+		assert isinstance(stmnt.base, CFunc) # TODO: also func ptrs etc
+		a = ast.Call()
+		a.func = ast.Name(id=stmnt.base.name)
+		a.args = map(lambda arg: astForStatement(funcEnv, arg), stmnt.args)
+		return a
+	else:
+		assert False, "cannot handle " + str(stmnt)
+
+def astForCStatement(funcEnv, stmnt):
+	assert isinstance(stmnt, CStatement)
+	if stmnt._leftexpr is None: # prefixed only
+		rightAstNode = astForStatement(funcEnv, stmnt._rightexpr)
+		if stmnt._op.content == "++":
+			return makeAstNodeCall(helper_prefixInc, rightAstNode)
+		elif stmnt._op.content == "--":
+			return makeAstNodeCall(helper_prefixDec, rightAstNode)
+		elif stmnt._op.content in OpUnary:
+			a = ast.UnaryOp()
+			a.op = OpUnary[stmnt._op.content]()
+			a.operand = rightAstNode
+			return a
+		else:
+			assert False, "unary prefix op " + str(stmnt._op) + " is unknown"
+	if stmnt._op is None:
+		return astForStatement(funcEnv, stmnt._leftexpr)
+	if stmnt._rightexpr is None:
+		leftAstNode = astForStatement(funcEnv, stmnt._leftexpr)
+		if stmnt._op.content == "++":
+			return makeAstNodeCall(helper_postfixInc, leftAstNode)
+		elif stmnt._op.content == "--":
+			return makeAstNodeCall(helper_postfixDec, leftAstNode)
+		else:
+			assert False, "unary postfix op " + str(stmnt._op) + " is unknown"
+	leftAstNode = astForStatement(funcEnv, stmnt._leftexpr)
+	rightAstNode = astForStatement(funcEnv, stmnt._rightexpr)
+	if stmnt._op.content in OpBin:
+		a = ast.BinOp()
+		a.op = OpBin[stmnt._op.content]()
+		a.left = leftAstNode
+		a.right = rightAstNode
+		return a
+	elif stmnt._op.content in OpBinBool:
+		a = ast.BoolOp()
+		a.op = OpBinBool[stmnt._op.content]()
+		a.values = [leftAstNode, rightAstNode]
+		return a
+	elif stmnt._op.content in OpBinCmp:
+		a = ast.Compare()
+		a.ops = [OpBinCmp[stmnt._op.content]()]
+		a.left = leftAstNode
+		a.comparators = [rightAstNode]
+		return a
+	elif stmnt._op.content == "=":
+		a = ast.Assign()
+		a.targets = [leftAstNode]
+		a.value = rightAstNode
+		return a
+	elif stmnt._op.content in OpAugAssign:
+		a = ast.AugAssign()
+		a.op = OpAugAssign[stmnt._op.content]()
+		a.target = leftAstNode
+		a.value = rightAstNode
+		return a
+	elif stmnt._op.content == "?:":
+		middleAstNode = astForStatement(funcEnv, stmnt._middleexpr)
+		a = ast.IfExp()
+		a.test = leftAstNode
+		a.body = middleAstNode
+		a.orelse = rightAstNode
+		return a
+	else:
+		assert False, "binary op " + str(stmnt._op) + " is unknown"
+
+def astForCWhile(funcEnv, stmnt):
+	assert isinstance(stmnt, CWhileStatement)
+	assert len(c.args) == 1
+	# TODO ...
+	pass
 
 class Interpreter:
 	def __init__(self):
@@ -116,8 +283,12 @@ class Interpreter:
 		for c in func.body.contentlist:
 			if isinstance(c, CVarDecl):
 				base.registerNewVar(c.name, c)
+			elif isinstance(c, CStatement):
+				base.astNode.body.append(astForCStatement(base, c))
+			elif isinstance(c, CWhileStatement):
+				base.astNode.body.append(astForCWhile(base, c))
 			else:
-				assert False, "cannot handle " + str(c) + " yet"
+				assert False, "cannot handle " + str(c)
 		base.popScope()
 		return base
 	
