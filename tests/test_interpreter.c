a666f968ae29d55081a4dc1711b1756c61b94bb4
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-07-24 01:57:23 +0200

    added test for interpreter

---
+++ tests/test_interpreter.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+	fprintf(stdout, "Hello world!\n");
+	return 0;
+}
---
+++ tests/test_interpreter.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+# Test interpreter
+# by Albert Zeyer, 2011
+# code under GPL
+
+import sys, os, os.path
+if __name__ == '__main__':
+	MyDir = os.path.dirname(sys.argv[0]) or "."
+else:
+	MyDir = "."
+
+sys.path.append(MyDir + "/../..") # so that 'import cparser' works as expected
+sys.path.append(MyDir + "/..") # so that 'import better_exchook' works
+
+import better_exchook
+better_exchook.install()
+
+import cparser
+
+def prepareState():
+	state = cparser.State()
+	state.autoSetupSystemMacros()	
+	state.autoSetupGlobalIncludeWrappers()
+	return state
+
+state = prepareState()
+cparser.parse(MyDir + "/test_interpreter.c", state)
+
+import cparser.interpreter
+
+interpreter = cparser.interpreter.Interpreter()
+interpreter.register(state)
+interpreter.registerFinalize()
+
+if __name__ == '__main__':
+	print "erros so far:"
+	for m in state._errors:
+		print m
+	
+	for f in state.contentlist:
+		if not isinstance(f, cparser.CFunc): continue
+		if not f.body: continue
+		
+		print
+		print "parsed content of " + str(f) + ":"
+		for c in f.body.contentlist:
+			print c
+	
+	print
+	print "PyAST of Py_Main:"
+	interpreter.dumpFunc("main")
+	
+	print
+	interpreter.runFunc("main", len(sys.argv), sys.argv + [None])
+
