ec28dfab082160688914a772bc742c94223158e7
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-07-26 14:05:47 +0200

    extended interpreter test a bit

--- tests/test_interpreter.c
+++ tests/test_interpreter.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-	fprintf(stdout, "Hello world!\n");
-	fprintf(stdout, "expr1 = %i\n", -1);
-	fprintf(stdout, "expr2 = %i\n", 1 > -1);
+	printf("Hello %s! (via printf)\n", "world");
+	fprintf(stdout, "%s world%c (via fprintf)\n", "Hello", '!');
+	printf("expr1 = %i\n", -1);
+	printf("expr2 = %i\n", 1 > -1);
 	return 0;
 }
