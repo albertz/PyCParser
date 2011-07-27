cc2fd5ccd73343ba1030bb9269e8703b4173501f
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-07-27 15:00:46 +0200

    C func: fgets

--- globalincludewrappers.py
+++ globalincludewrappers.py
@@ -56,6 +56,7 @@ class Wrapper:
 		state.vars["stderr"] = CWrapValue(callCFunc("fdopen", 2, "a"))
 		wrapCFunc(state, "fprintf", restype=ctypes.c_int, argtypes=(FileP, ctypes.c_char_p))
 		wrapCFunc(state, "fputs", restype=ctypes.c_int, argtypes=(ctypes.c_char_p, FileP))
+		wrapCFunc(state, "fgets", restype=ctypes.c_char_p, argtypes=(ctypes.c_char_p, ctypes.c_int, FileP))
 		wrapCFunc(state, "fflush", restype=ctypes.c_int, argtypes=(FileP,))
 		state.vars["errno"] = CWrapValue(0) # TODO
 		state.macros["EOF"] = Macro(rightside="-1") # TODO?
