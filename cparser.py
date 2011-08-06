b5f80daa82cb958927d8ce2f9b06704f216ef3c9
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-08-06 12:58:59 +0200

    small fix. happened with prefixed expr

--- cparser.py
+++ cparser.py
@@ -2100,7 +2100,7 @@ class CStatement(_CBaseWithOptBody):
 		elif self._state == 8: # right-to-left chain, pull down
 			assert isinstance(self._rightexpr, CStatement)
 			self._rightexpr._cpre3_handle_token(stateStruct, token)
-			if self._rightexpr._state in (5,7):
+			if self._rightexpr._state in (5,7,9):
 				self._state = 9
 		elif self._state == 9: # right-to-left chain after op + expr
 			assert isinstance(self._rightexpr, CStatement)
