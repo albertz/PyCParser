c74aef954e41fcb54dd9b0c6e83d8c6782fc9fe4
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2012-08-13 02:46:31 +0200

    fix up tabs

--- tests/test_funcarraydecl.py
+++ tests/test_funcarraydecl.py
@@ -4,15 +4,15 @@ from pprint import pprint
 import cparser, test
 
 testcode = """
-    int16_t (*motion_val[2])[2];
+	int16_t (*motion_val[2])[2];
 """
 
 state = test.newState(testcode)
 
 def test():
-    cparser.parse("test.c", state)
-    pprint(state._errors)
-    
+	cparser.parse("test.c", state)
+	pprint(state._errors)
+	
 if __name__ == '__main__':
-    test()
-    
+	test()
+	
