da8700883e6a644242f5e8d4d66db53289322541
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2012-08-13 04:32:07 +0200

    more testing

---
+++ tests/test_funcptrdecl.py
@@ -0,0 +1,31 @@
+import sys
+sys.path += [".."]
+from pprint import pprint
+from cparser import *
+import test
+
+testcode = """
+	int16_t (*f)();
+	int16_t (*g)(char a, void*);
+"""
+
+state = test.parse(testcode)	
+
+f = state.vars["f"]
+g = state.vars["g"]
+
+assert f.name == "f"
+assert isinstance(f.type, CFuncPointerDecl)
+assert f.type.type == CStdIntType("int16_t")
+assert f.type.args == []
+
+assert isinstance(g.type, CFuncPointerDecl)
+gargs = g.type.args
+assert isinstance(gargs, list)
+assert len(gargs) == 2
+assert isinstance(gargs[0], CFuncArgDecl)
+assert gargs[0].name == "a"
+assert gargs[0].type == CBuiltinType(("char",))
+assert gargs[1].name is None
+assert gargs[1].type == CBuiltinType(("void","*"))
+
