7ca496838a941866d905dc1d57c119f2a1da72ac
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2012-08-13 02:45:19 +0200

    initial testing code

---
+++ tests/test.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import sys, os
+mydir = os.path.dirname(__file__)
+sys.path += [mydir + "/.."]
+
+import cparser
+
+def main():
+    import types
+    
+    from glob import glob
+    for f in glob(mydir + "/test_*.py"):
+        c = compile(open(f).read(), os.path.basename(f), "exec")
+        m = {}
+        eval(c, m)
+        for name,obj in m.items():
+            if name.startswith("test") and isinstance(obj, types.FunctionType):
+                obj()
+
+def newState(testcode, testfn = "test.c"):
+    state = cparser.State()
+    state.autoSetupSystemMacros()
+    
+    origReadLocal = state.readLocalInclude
+    def readLocalIncludeWrapper(fn):
+        if fn == testfn:
+    		def reader():
+                for c in testcode:
+                    yield c
+    		reader = reader()
+    		return reader, fn
+        return origReadLocal(fn)
+    state.readLocalInclude = readLocalIncludeWrapper
+    
+    return state
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_funcarraydecl.py
@@ -0,0 +1,18 @@
+import sys
+sys.path += [".."]
+from pprint import pprint
+import cparser, test
+
+testcode = """
+    int16_t (*motion_val[2])[2];
+"""
+
+state = test.newState(testcode)
+
+def test():
+    cparser.parse("test.c", state)
+    pprint(state._errors)
+    
+if __name__ == '__main__':
+    test()
+    
