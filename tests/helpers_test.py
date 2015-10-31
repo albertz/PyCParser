add49ddc0cca0ca7327f0aa3ede64dbfa39c2351
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2015-10-31 16:04:40 +0100

    test helpers cleanup

--- tests/helpers_test.py
+++ tests/helpers_test.py
@@ -7,27 +7,12 @@ better_exchook.replace_traceback_format_tb()
 import cparser
 from pprint import pprint
 
-def newState(testcode, testfn = "test.c", withSystemMacros=True, withGlobalIncludeWrappers=False):
+
+def parse(testcode, withSystemMacros=True, withGlobalIncludeWrappers=False):
 	state = cparser.State()
 	if withSystemMacros: state.autoSetupSystemMacros()
 	if withGlobalIncludeWrappers: state.autoSetupGlobalIncludeWrappers()
-		
-	origReadLocal = state.readLocalInclude
-	def readLocalIncludeWrapper(fn):
-		if fn == testfn:
-			def reader():
-				for c in testcode:
-					yield c
-			reader = reader()
-			return reader, fn
-		return origReadLocal(fn)
-	state.readLocalInclude = readLocalIncludeWrapper
-	
-	return state
-
-def parse(testcode, **kwargs):
-	state = newState(testcode, **kwargs)
-	cparser.parse("test.c", state)
+	cparser.parse_code(testcode, state)
 	if state._errors:
 		print "parsing errors:"
 		pprint(state._errors)
