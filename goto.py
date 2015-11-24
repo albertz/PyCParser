2b12a5dbb09e848282dcebe4a4118e93b50da527
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2015-11-24 18:46:24 +0100

    goto

--- goto.py
+++ goto.py
@@ -1,4 +1,6 @@
 
+import ast
+
 
 # We could use this funny little hack:
 # background: http://stackoverflow.com/questions/6959360/goto-in-python
@@ -95,6 +97,9 @@ def goto(fn):
 # much more complicated route:
 # - First, we flatten any Python AST into a series of statements,
 #   with even more goto's added for while/for loops, if's, etc.
+#   (We can avoid doing this for all sub-ASTs where there are no
+#    no goto-labels. The goal is to have all goto-labels at
+#    the top-level, not inside a sub-AST.)
 # - Only conditional goto's can stay.
 # - All Gotos and Goto-labels are marked somehow as special elements.
 # So, we end up with sth like:
@@ -117,3 +122,147 @@ def goto(fn):
 #   `if goto == <goto-label>: goto = None`
 # - For every goto-statement, we add this code:
 #   `goto = <goto-stmnt>; continue`
+
+
+class GotoLabel:
+	def __init__(self, label):
+		self.label = label
+
+
+class GotoStatement:
+	def __init__(self, label):
+		self.label = label
+
+
+class _Flatten:
+	def __init__(self):
+		self.c = 1
+
+	def make_jump(self):
+		label = self.c
+		self.c += 1
+		return GotoStatement(label)
+
+	def flatten(self, body):
+		"""
+		:type body: list[ast.AST]
+		:rtype: list[ast.AST]
+		"""
+		r = []
+		for s in body:
+			if isinstance(s, ast.If):
+				a = ast.UnaryOp()
+				a.op = ast.Not()
+				a.operand = s.test
+				goto_final_stmnt = self.make_jump()
+				r += [ast.If(test=a, body=[goto_final_stmnt], orelse=[])]
+				r += self.flatten(s.body)
+				r += [GotoLabel(goto_final_stmnt.label)]
+			elif isinstance(s, ast.While):
+				if s.orelse: raise NotImplementedError
+				goto_repeat_stmnt = self.make_jump()
+				r += [GotoLabel(goto_repeat_stmnt.label)]
+				a = ast.UnaryOp()
+				a.op = ast.Not()
+				a.operand = s.test
+				goto_final_stmnt = self.make_jump()
+				r += [ast.If(test=a, body=[goto_final_stmnt], orelse=[])]
+				r += self.flatten(s.body)
+				r += [goto_repeat_stmnt]
+				r += [GotoLabel(goto_final_stmnt.label)]
+			elif isinstance(s, ast.For):
+				raise NotImplementedError
+			elif isinstance(s, (ast.TryExcept, ast.TryFinally)):
+				raise NotImplementedError
+			else:
+				r += [s]
+		return r
+
+
+def _ast_for_value(v):
+	if isinstance(v, str): return ast.Str(s=v)
+	elif isinstance(v, int): return ast.Num(n=v)
+	else: raise NotImplementedError("type (%r) %r" % (type(v), v))
+
+
+class _HandleGoto:
+
+	def __init__(self, gotoVarName):
+		self.gotoVarName = gotoVarName
+
+	def handle_goto_stmnt(self, stmnt):
+		assert isinstance(stmnt, GotoStatement)
+		a = ast.Assign()
+		a.targets = [ast.Name(id=self.gotoVarName, ctx=ast.Store())]
+		a.value = _ast_for_value(stmnt.label)
+		return [a, ast.Continue()]
+
+	def handle_goto_label(self, stmnt):
+		assert isinstance(stmnt, GotoLabel)
+		reset_ast = ast.Assign()
+		reset_ast.targets = [ast.Name(id=self.gotoVarName, ctx=ast.Store())]
+		reset_ast.value = ast.Name(id="None", ctx=ast.Load())
+		test_ast = ast.Compare()
+		test_ast.ops = [ast.Eq()]
+		test_ast.left = ast.Name(id=self.gotoVarName, ctx=ast.Store())
+		test_ast.comparators = [_ast_for_value(stmnt.label)]
+		return [ast.If(test=test_ast, body=[reset_ast], orelse=[])]
+
+	def handle_body(self, body):
+		"""
+		:type body: list[ast.AST]
+		:rtype: list[ast.AST]
+		"""
+		parts = [[]]
+		for s in body:
+			if isinstance(s, GotoLabel):
+				parts += [s, []]
+			else:
+				parts[-1].append(s)
+		r = []
+		for l in parts:
+			if isinstance(l, GotoLabel):
+				r += self.handle_goto_label(l)
+			else:
+				sr = []
+				for s in l:
+					if isinstance(s, ast.If):
+						assert not s.orelse
+						assert len(s.body) == 1
+						assert isinstance(s.body[0], GotoStatement)
+						sr += [ast.If(test=s.test, orelse=[],
+									  body=self.handle_goto_stmnt(s.body[0]))]
+					elif isinstance(s, (ast.While, ast.For)):
+						assert False, "not expected: %r" % s
+					elif isinstance(s, GotoStatement):
+						sr += self.handle_goto_stmnt(s)
+					else:
+						sr += [s]
+				test_ast = ast.Compare()
+				test_ast.ops = [ast.Is()]
+				test_ast.left = ast.Name(id=self.gotoVarName, ctx=ast.Store())
+				test_ast.comparators = [ast.Name(id="None", ctx=ast.Load())]
+				r += [ast.If(test=test_ast, body=sr, orelse=[])]
+		return r
+
+	def wrap_func_body(self, flat_body):
+		var_ast = ast.Assign()
+		var_ast.targets = [ast.Name(id=self.gotoVarName, ctx=ast.Store())]
+		var_ast.value = ast.Name(id="None", ctx=ast.Load())
+		main_loop_ast = ast.While(orelse=[])
+		main_loop_ast.test = ast.Name(id="True", ctx=ast.Load())
+		main_loop_ast.body = self.handle_body(flat_body)
+		main_loop_ast.body += [ast.Break()]
+		return [var_ast, main_loop_ast]
+
+
+def transform_goto(f, gotoVarName):
+	assert isinstance(f, ast.FunctionDef)
+	flat_body = _Flatten().flatten(f.body)
+	new_body = _HandleGoto(gotoVarName).wrap_func_body(flat_body)
+	new_func_ast = ast.FunctionDef(
+		name=f.name,
+		args=f.args,
+		decorator_list=f.decorator_list,
+		body=new_body)
+	return new_func_ast
---
+++ tests/test_goto.py
@@ -0,0 +1,82 @@
+
+from nose.tools import assert_equal, assert_is_instance, assert_in, assert_greater, assert_true, assert_false
+
+import ast
+import goto
+
+def unparse(pyAst):
+	from cStringIO import StringIO
+	output = StringIO()
+	from py_demo_unparse import Unparser
+	Unparser(pyAst, output)
+	output.write("\n")
+	return output.getvalue()
+
+def parse(s):
+	return ast.parse(s)
+
+def get_indent_prefix(s):
+	return s[:len(s) - len(s.lstrip())]
+
+def get_same_indent_prefix(lines):
+	if not lines: return ""
+	prefix = get_indent_prefix(lines[0])
+	if not prefix: return ""
+	if all([l.startswith(prefix) for l in lines]):
+		return prefix
+	return None
+
+def remove_indent_lines(s):
+	if not s: return ""
+	lines = s.splitlines(True)
+	prefix = get_same_indent_prefix(lines)
+	if prefix is None:  # not in expected format. just lstrip all lines
+		return "".join([l.lstrip() for l in lines])
+	return "".join([l[len(prefix):] for l in lines])
+
+def fix_code(s):
+	if not s: return ""
+	if not s[0]: return fix_code(s[1:])
+	s = remove_indent_lines(s[1:])
+	s = s.replace("\t", " " * 4)
+	return s
+
+def test_parse_unparse():
+	s = """
+	def foo():
+		pass
+	"""
+	s = fix_code(s)
+	print s
+	a = parse(s)
+	ss = unparse(a)
+	assert_equal(s.strip(), ss.strip())
+
+def test_transform_goto():
+	s = """
+	def foo():
+		i = 0
+		if i == 5: return i
+		i += 1
+		print "hello"
+	"""
+	s = fix_code(s)
+	print s
+	m = parse(s)
+	assert isinstance(m, ast.Module)
+	assert len(m.body) == 1
+	f = m.body[0]
+	assert isinstance(f, ast.FunctionDef)
+	assert len(f.body) == 4
+	f.body = f.body[:1] + [goto.GotoLabel("label")] + f.body[1:] + [goto.GotoStatement("label")]
+	f = goto.transform_goto(f, "goto")
+	ss = unparse(f)
+	print ss
+
+	c = compile(ss, "<src>", "single")
+	print c
+	d = {}
+	exec c in d, d
+	func = d["foo"]
+	r = func()
+	assert r == 5
