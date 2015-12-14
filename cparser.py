da2114ca530a35cf9ac3a0c1bccaf8a1bec66f71
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2015-12-14 19:53:58 +0100

    getValueType, handle sizeof()

--- cparser.py
+++ cparser.py
@@ -2180,6 +2180,8 @@ def getValueType(stateStruct, obj):
 			base_type = base_type.type
 		assert isinstance(base_type, (CFuncPointerDecl,CFunc))
 		return base_type.type  # return-type
+	if isinstance(obj, CSizeofSymbol):
+		return CStdIntType("size_t")
 	if isinstance(obj, CStr):
 		return CArrayType(arrayOf=CBuiltinType(("char",)), arrayLen=CNumber(len(obj.content) + 1))
 	if isinstance(obj, CChar):
