d55565ef1b17c5274628a56043fdba7cdec69b43
Albert Zeyer <albzey@gmail.com>
2019-09-13 00:07:01 +0200

    test interpreter

--- demos/test_interpreter.py
+++ demos/test_interpreter.py
@@ -1,16 +1,21 @@
-#!/usr/bin/python
-# Test interpreter
-# by Albert Zeyer, 2011
-# code under GPL
+#!/usr/bin/env python3
+
+"""
+Test interpreter
+by Albert Zeyer, 2011
+code under GPL
+"""
+
+from __future__ import print_function
 
 import sys, os, os.path
+
 if __name__ == '__main__':
     MyDir = os.path.dirname(sys.argv[0]) or "."
 else:
     MyDir = "."
 
-sys.path.append(MyDir + "/../..") # so that 'import cparser' works as expected
-sys.path.append(MyDir + "/..") # so that 'import better_exchook' works
+sys.path.append(MyDir + "/..")
 
 import better_exchook
 better_exchook.install()
@@ -26,29 +31,30 @@ def prepareState():
 state = prepareState()
 cparser.parse(MyDir + "/test_interpreter.c", state)
 
-import cparser.interpreter
+import interpreter
 
-interpreter = cparser.interpreter.Interpreter()
+interpreter = interpreter.Interpreter()
 interpreter.register(state)
 
+
 if __name__ == '__main__':
-    print "erros so far:"
+    print("errors so far:")
     for m in state._errors:
-        print m
+        print(m)
 
     for f in state.contentlist:
         if not isinstance(f, cparser.CFunc): continue
         if not f.body: continue
 
-        print
-        print "parsed content of " + str(f) + ":"
+        print()
+        print("parsed content of " + str(f) + ":")
         for c in f.body.contentlist:
-            print c
+            print(c)
 
-    print
-    print "PyAST of main:"
+    print()
+    print("PyAST of main:")
     interpreter.dumpFunc("main")
 
-    print
-    print
+    print()
+    print()
     interpreter.runFunc("main", len(sys.argv), sys.argv + [None])
